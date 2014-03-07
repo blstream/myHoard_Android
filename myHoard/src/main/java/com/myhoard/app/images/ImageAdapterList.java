4ddc724c5b9daf494606b9ed60b6bd83c382045b
pbrzozowski1992 <p_brzozowski@hotmail.com>
2014-03-07 18:14:08 +0100

    [MOD] Change BaseAdapter to CursorAdapter

--- myHoard/src/main/java/com/myhoard/app/images/ImageAdapterList.java
+++ myHoard/src/main/java/com/myhoard/app/images/ImageAdapterList.java
@@ -1,77 +1,54 @@
 package com.myhoard.app.images;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.support.v4.widget.CursorAdapter;
 import com.myhoard.app.R;
 
-
-import java.util.ArrayList;
 
 /**
  * Created by Piotr Brzozowski on 01.03.14.
  * ImageAdapterList class used to get list of element from data in SearchFragment
  */
 
-// FIXME Adapter w tym przypadku powinien być dziedziczony po CUrsorAdapter
-public class ImageAdapterList extends BaseAdapter {
-    //Context object to get context of application
-    private Context mContext;
-    //ArrayList for collection element name from database
-    private ArrayList<String> mCollectionElementName;
-    //ArrayList for collection element avatar from database
-    private ArrayList<String> mCollectionElementAvatar;
-    //Constructor get context of application, ArrayList for collection element name, ArrayList for collection element avatar
-    public ImageAdapterList(Context context, ArrayList<String> collectionElementName, ArrayList<String> collectionElementAvatar){
-        //set private variables of class
-        mContext = context;
-        mCollectionElementName = collectionElementName;
-        mCollectionElementAvatar = collectionElementAvatar;
+public class ImageAdapterList extends CursorAdapter {
+
+
+    public ImageAdapterList(Context context, Cursor c, int flags) {
+        super(context, c, flags);
     }
 
     @Override
-    public int getCount() {
-        return mCollectionElementName.size();
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View v = inflater.inflate(R.layout.search_listitem,parent,false);
+        bindView(v,context,cursor);
+        return v;
     }
 
     @Override
-    public Object getItem(int position) {
-        return mCollectionElementName.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        //Get LayoutInflater object to create view
-        LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        //New view for each of list elements matched to class
-        View listView = inflater.inflate(R.layout.search_listitem,null);
+    public void bindView(View view, Context context, Cursor cursor) {
         //Get TextView object from ListView
-        assert listView != null;
-        TextView textViewCollectionElementName = (TextView)listView.findViewById(R.id.textViewListRowSearch);
+        TextView textViewCollectionElementName = (TextView)view.findViewById(R.id.textViewListRowSearch);
         //Get ImageView object from ListView
-        ImageView imageViewCollectionElementAvatar = (ImageView)listView.findViewById(R.id.imageViewListRowSearch);
+        ImageView imageViewCollectionElementAvatar = (ImageView)view.findViewById(R.id.imageViewListRowSearch);
         //Set text for TextView in search_listitem
-        textViewCollectionElementName.setText(mCollectionElementName.get(position));
+        textViewCollectionElementName.setText(cursor.getString(0));
         //Set image for ImageView in search_listitem
-        if(mCollectionElementAvatar.get(position)==null){
-            imageViewCollectionElementAvatar.setImageResource(R.drawable.ic_launcher);
+        if(cursor.getString(1)==null){
+            imageViewCollectionElementAvatar.setImageResource(R.drawable.nophoto);
         }
         else{
-            imageViewCollectionElementAvatar.setImageBitmap(decodeSampledBitmapFromResource(mCollectionElementAvatar.get(position),100,100));
+            imageViewCollectionElementAvatar.setImageBitmap(decodeSampledBitmapFromResource(cursor.getString(1),100,100));
         }
-        return listView;
+
     }
 
     public static int calculateInSampleSize(
