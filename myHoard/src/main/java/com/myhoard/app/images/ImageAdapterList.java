aee1c0a95d93064f33f9eb46808276f071b86e87
pbrzozowski1992 <p_brzozowski@hotmail.com>
2014-03-16 18:38:18 +0100

    [MOD] ImageAdapterList - load bitmap asynchronously

--- myHoard/src/main/java/com/myhoard/app/images/ImageAdapterList.java
+++ myHoard/src/main/java/com/myhoard/app/images/ImageAdapterList.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.myhoard.app.R;
+
+import java.lang.ref.WeakReference;
 
 
 /**
@@ -45,8 +48,10 @@ public class ImageAdapterList extends CursorAdapter {
 		if (cursor.getString(1) == null) {
 			imageViewCollectionElementAvatar.setImageResource(R.drawable.nophoto);
 		} else {
-			imageViewCollectionElementAvatar.setImageBitmap(decodeSampledBitmapFromResource(cursor.getString(1), 100, 100));
-		}
+			//imageViewCollectionElementAvatar.setImageBitmap(decodeSampledBitmapFromResource(cursor.getString(1), 100, 100));
+            //Load bitmap asynchronously
+		    loadBitmap(cursor.getString(1),imageViewCollectionElementAvatar);
+        }
 
 	}
 
@@ -85,5 +90,36 @@ public class ImageAdapterList extends CursorAdapter {
 
 		return inSampleSize;
 	}
+    public void loadBitmap(String path, ImageView imageView) {
+        BitmapWorkerTask task = new BitmapWorkerTask(imageView);
+        task.execute(path);
+    }
+    class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
+        private final WeakReference<ImageView> imageViewReference;
+        private String data;
+
+        public BitmapWorkerTask(ImageView imageView) {
+            // Use a WeakReference to ensure the ImageView can be garbage collected
+            imageViewReference = new WeakReference<>(imageView);
+        }
+
+        // Decode image in background.
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            data = params[0];
+            return decodeSampledBitmapFromResource(data, 100, 100);
+        }
+
+        // Once complete, see if ImageView is still around and set bitmap.
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap != null) {
+                final ImageView imageView = imageViewReference.get();
+                if (imageView != null) {
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
+    }
 }
 
