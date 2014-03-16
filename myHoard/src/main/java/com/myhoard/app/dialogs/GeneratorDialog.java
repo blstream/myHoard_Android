56b1983b9385fcdc73cb0c92edae74c619436d45
pbrzozowski1992 <p_brzozowski@hotmail.com>
2014-03-16 16:09:58 +0100

    [FIX] problem with generate elements of collection

--- myHoard/src/main/java/com/myhoard/app/dialogs/GeneratorDialog.java
+++ myHoard/src/main/java/com/myhoard/app/dialogs/GeneratorDialog.java
@@ -8,9 +8,6 @@ import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,24 +25,27 @@ import java.util.Random;
  * Created by Piotr Brzozowski on 15.03.14.
  * Dialog use choose number of collections and elements to generate
  */
-public class GeneratorDialog extends DialogFragment implements View.OnClickListener, LoaderManager.LoaderCallbacks<Cursor> {
+public class GeneratorDialog extends DialogFragment implements View.OnClickListener {
+
     private EditText collectionNumber;
     private EditText elementNumber;
-    private Button cancelButton;
-    private Button generateButton;
-    private String collection_name;
-    private String object_id;
-    private String element_name;
+    private String collection_id;
+    private String element_id;
     private Context mContext;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //Create dialog to generate data
         Dialog dialog = new Dialog(getActivity());
         dialog.setContentView(R.layout.generator_dialog);
-        mContext = getActivity();
+        //Set mContext
+        mContext = getActivity().getApplicationContext();
+        //Set EditText for dialog
         collectionNumber = (EditText)dialog.findViewById(R.id.editTextGeneratorDialog);
         elementNumber = (EditText)dialog.findViewById(R.id.editText2GeneratorDialog);
-        cancelButton = (Button)dialog.findViewById(R.id.buttonGeneratorDialog);
-        generateButton = (Button)dialog.findViewById(R.id.button2GeneratorDialog);
+        //Set Button for dialog
+        Button cancelButton = (Button) dialog.findViewById(R.id.buttonGeneratorDialog);
+        Button generateButton = (Button) dialog.findViewById(R.id.button2GeneratorDialog);
+        //Set interact with cancel and generate button
         cancelButton.setOnClickListener(this);
         generateButton.setOnClickListener(this);
         return dialog;
@@ -53,11 +53,14 @@ public class GeneratorDialog extends DialogFragment implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        //Get id of clicked button
         switch(v.getId()){
             case R.id.buttonGeneratorDialog:
+                //Cancel button
                 dismiss();
                 break;
             case R.id.button2GeneratorDialog:
+                //Generate button
                 generateData();
                 dismiss();
                 break;
@@ -66,29 +69,41 @@ public class GeneratorDialog extends DialogFragment implements View.OnClickListe
 
     public void generateData() {
         assert collectionNumber.getText() != null;
+        //Get text from Collection number label
         int collection_number = Integer.parseInt(collectionNumber.getText().toString());
         assert elementNumber.getText() != null;
+        //Get text from Element number label
         int element_number = Integer.parseInt(elementNumber.getText().toString());
         InputStream is = null;
-        String[] list = new String[]{"flower1.jpg","flower2.jpg","beer1.jpg","snack1.jpg"};
+        //List of jpg images in assets folder
+        String[] list = new String[]{"flower1.jpg","flower2.jpg","beer1.jpg","snack1.jpg",
+                "ball1.jpg","coins1.jpg","pizza1.jpg","pizza2.jpg","snack2.jpg"};
         OutputStream file_stream = null;
+        //asyncHandler use to do insert, asynchronous
         AsyncQueryHandler asyncHandler =
                 new AsyncQueryHandler(getActivity().getContentResolver()) {
                 };
+        //Generate collection and element of this collection
         for (int i = 0; i < collection_number; i++) {
             ContentValues valuesCollection = new ContentValues();
+            String collection_name;
+            //Put value about collection to valuesCollection
             valuesCollection.put(DataStorage.Collections.NAME, collection_name = generateString(new Random(), "abcdefgijklmnouprstwuxyz", 10));
             valuesCollection.put(DataStorage.Collections.DESCRIPTION, generateString(new Random(), "abcdefgijklmnouprstwuxyz", 20));
+            //AssetManager used to get data from image in assets folder
             AssetManager am = getActivity().getAssets();
             Random r = new Random();
             int random_number = r.nextInt(list.length);
+            //Get random image from assets folder
             try {
                 is = am.open(list[random_number]);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            File file = new File(getActivity().getFilesDir(), list[random_number]);
+            //Create new file in mContext.getFilesDir() path
+            File file = new File(mContext.getFilesDir(), list[random_number]);
             try {
+                //New stream used to write data in file
                 file_stream = new FileOutputStream(file);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -99,32 +114,60 @@ public class GeneratorDialog extends DialogFragment implements View.OnClickListe
                 assert is != null;
                 while ((length = is.read(b)) != -1) {
                     assert file_stream != null;
+                    //Write byte structure to file
                     file_stream.write(b, 0, length);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
             assert getActivity().getFilesDir() != null;
+            //Put avatar file path to contentValues object
             valuesCollection.put(DataStorage.Collections.AVATAR_FILE_NAME, getActivity().getFilesDir().getPath() + "/" + list[random_number]);
+            //Insert data of collection to database asynchronously
             asyncHandler.startInsert(-1, null, DataStorage.Collections.CONTENT_URI, valuesCollection);
             for (int j = 0; j < element_number; j++) {
                 ContentValues valuesElement = new ContentValues();
-                getLoaderManager().initLoader(0, null, this);
+                //Get id of last collection added to database
+                Cursor cursor = mContext.getContentResolver().query(DataStorage.Collections.CONTENT_URI,
+                        new String[]{DataStorage.Collections._ID},
+                        DataStorage.Collections.NAME+" = '"+ collection_name +"'",null,null);
+                assert cursor != null;
+                cursor.moveToFirst();
+                while(!cursor.isAfterLast()){
+                    collection_id=cursor.getString(0);
+                    cursor.moveToNext();
+                }
+                String element_name;
+                //Put values about element to valuesElement object
                 valuesElement.put(DataStorage.Items.NAME, element_name = generateString(new Random(), "abcdefgijklmnouprstwuxyz", 6));
                 valuesElement.put(DataStorage.Items.DESCRIPTION, generateString(new Random(), "abcdefgijklmnouprstwuxyz", 15));
-                valuesElement.put(DataStorage.Items.ID_COLLECTION, object_id);
+                valuesElement.put(DataStorage.Items.ID_COLLECTION, collection_id);
+                //Insert data of collection element to database asynchronously
                 asyncHandler.startInsert(1, null, DataStorage.Items.CONTENT_URI, valuesElement);
-                getLoaderManager().restartLoader(1,null,this);
-                am = getActivity().getAssets();
+                //Get id of added element to database
+                Cursor cursor1 = mContext.getContentResolver().query(DataStorage.Items.CONTENT_URI,
+                        new String[]{DataStorage.Items.TABLE_NAME+"."+DataStorage.Items._ID,DataStorage.Media.AVATAR},
+                        DataStorage.Items.NAME+" = '"+ element_name +"'",null,null);
+                assert cursor1 != null;
+                cursor1.moveToFirst();
+                while(!cursor1.isAfterLast()){
+                    element_id=cursor1.getString(0);
+                    cursor1.moveToNext();
+                }
+                am = mContext.getAssets();
                 r = new Random();
+                //Get random image from assets folder
                 random_number = r.nextInt(list.length);
                 try {
+                    //Open image
                     is = am.open(list[random_number]);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                file = new File(getActivity().getFilesDir(), list[random_number]);
+                //Create new file in mContext.getFilesDir() path
+                file = new File(mContext.getFilesDir(), list[random_number]);
                 try {
+                    //New stream used to write data in file
                     file_stream = new FileOutputStream(file);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
@@ -132,55 +175,31 @@ public class GeneratorDialog extends DialogFragment implements View.OnClickListe
                 try {
                     while ((length = is.read(b)) != -1) {
                         assert file_stream != null;
+                        //Write byte structure to file
                         file_stream.write(b, 0, length);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 ContentValues valuesMedia = new ContentValues();
-                valuesMedia.put(DataStorage.Media.ID_ITEM,object_id);
-                valuesMedia.put(DataStorage.Media.AVATAR,getActivity().getFilesDir().getPath() + "/" + list[random_number]);
+                //Put data of media to valuesMedia object
+                valuesMedia.put(DataStorage.Media.ID_ITEM,element_id);
+                assert mContext.getFilesDir() !=null;
+                valuesMedia.put(DataStorage.Media.AVATAR,mContext.getFilesDir().getPath() + "/" + list[random_number]);
+                //Insert data of element media to database asynchronously
                 asyncHandler.startInsert(-1,null,DataStorage.Media.CONTENT_URI,valuesMedia);
             }
         }
     }
+
     public String generateString(Random rng, String characters, int length)
     {
         char[] text = new char[length];
         for (int i = 0; i < length; i++)
         {
+            //Generate random string for element/collection name, description
             text[i] = characters.charAt(rng.nextInt(characters.length()));
         }
         return new String(text);
-    }
-
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        CursorLoader cursor_loader = null;
-        switch(id){
-            case 0:
-                cursor_loader = new CursorLoader(mContext, DataStorage.Collections.CONTENT_URI,
-                        new String[]{DataStorage.Collections._ID},
-                        DataStorage.Collections.NAME+" = '"+collection_name+"'",null,null);
-                break;
-            case 1:
-                cursor_loader = new CursorLoader(mContext, DataStorage.Items.CONTENT_URI,
-                        new String[]{DataStorage.Items.TABLE_NAME+"."+DataStorage.Items._ID},
-                        DataStorage.Items.NAME+" = '"+element_name+"'",null,null);
-                break;
-        }
-        return cursor_loader;
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        cursor.moveToFirst();
-        object_id = cursor.getString(0);
-        cursor.close();
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
     }
 }
