f400408d8b4d8532398c12cd60bfd320691fb8e9
gohiluk <tomekknosal@gmail.com>
2014-03-18 20:31:00 +0100

    [ADD] HttpEngine

---
+++ myHoard/src/main/java/com/myhoard/app/model/Token.java
@@ -0,0 +1,43 @@
+package com.myhoard.app.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Description
+ *
+ * @author gohilukk
+ *         Date: 18.03.14
+ */
+ public class Token {
+
+    @SerializedName("access_token")
+    private String accessToken;
+    @SerializedName("expires_in")
+    private int expiresIn;
+    @SerializedName("refresh_token")
+    private String refreshToken;
+
+    public String getAccess_token() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String access_token) {
+        this.accessToken = access_token;
+    }
+
+    public int getExpiresTn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(int expires_in) {
+        this.expiresIn = expires_in;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refresh_token) {
+        this.refreshToken = refresh_token;
+    }
+}
