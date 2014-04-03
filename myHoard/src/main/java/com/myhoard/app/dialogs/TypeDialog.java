651a84d4efa0dc6154e623dada4c51a5b4aad12c
Rafał Soudani <rsoudani@gmail.com>
2014-04-03 14:29:39 +0200

    [FIX] Use style in Type Dialog (FIXME)

--- myHoard/src/main/java/com/myhoard/app/dialogs/TypeDialog.java
+++ myHoard/src/main/java/com/myhoard/app/dialogs/TypeDialog.java
@@ -1,15 +1,10 @@
 package com.myhoard.app.dialogs;
 
 import android.app.Dialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
 import com.myhoard.app.R;
 
@@ -17,7 +12,6 @@ import com.myhoard.app.R;
  * Created by Rafał Soudani on 14.03.2014
  */
 public class TypeDialog extends DialogFragment implements View.OnClickListener {
-    private RadioGroup rgType;
     private RadioButton rbPrivate;
     private RadioButton rbPublic;
     private RadioButton rbOffline;
@@ -25,22 +19,9 @@ public class TypeDialog extends DialogFragment implements View.OnClickListener {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final Dialog dialog = new Dialog(getActivity());
-        /* AWA:FIXME:Uzycie styli zamiast ustawiania w kodzie wygladu Dialogu
-        Proponuję sprawdzić czy nie da się tego ustawić w pliku stylu
-        i ewentualnei w kodzie zroibć tylko ustawianie tego stylu
-        Np.
-        http://stackoverflow.com/questions/19167185/cant-get-my-dialogfragment-background-to-be-transparent
-        */
-        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        final Dialog dialog = new Dialog(getActivity(), R.style.TypeDialog);
         dialog.setContentView(R.layout.type_dialog);
-        dialog.getWindow().setBackgroundDrawable(
-                new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
-
-        rgType = (RadioGroup) dialog.findViewById(R.id.rgType);
 
         rbOffline = (RadioButton) dialog.findViewById(R.id.rbOffline);
         rbPrivate = (RadioButton) dialog.findViewById(R.id.rbPrivate);
